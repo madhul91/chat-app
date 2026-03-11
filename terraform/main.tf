c634f87fc54de9ee44e6dd69af5c9aa0e211a265
madhul91 <madhuldubey06@gmail.com>
2026-03-11 11:03:33 +0000

    8

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ data "aws_subnets" "public_subnets" {
 
 
 resource "aws_security_group" "devops_sg" {
-  name   = "sg12355689"
+  name_prefix = "chat-app-sg-"
   vpc_id = data.aws_vpc.default.id
 
   ingress {
